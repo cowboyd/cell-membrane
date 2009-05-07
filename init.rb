7a79451f8cec5bfe447083095957a5ff6d0caf9b
Charles Lowell <cowboyd@thefrontside.net>
2009-05-07 10:19:13 -0400

    use a dir glob to copy files and not recursively, otherwise it messes with a source tree that is stored in SVN

--- init.rb
+++ init.rb
@@ -2,5 +2,8 @@
 Cell::Base
 require 'design'
 
-FileUtils.cp_r File.join(File.dirname(__FILE__), 'public', 'images', 'design'), "#{Rails.public_path}/images"
-FileUtils.cp_r File.join(File.dirname(__FILE__), 'public', 'images', 'pixels'), "#{Rails.public_path}/images" 
+FileUtils.mkdir_p "#{Rails.public_path}/images/design"
+FileUtils.mkdir_p "#{Rails.public_path}/images/pixels"
+
+FileUtils.cp Dir[File.join(File.dirname(__FILE__), 'public', 'images', 'design', '*')], "#{Rails.public_path}/images/design"
+FileUtils.cp Dir[File.join(File.dirname(__FILE__), 'public', 'images', 'pixels', '*')], "#{Rails.public_path}/images/pixels" 
